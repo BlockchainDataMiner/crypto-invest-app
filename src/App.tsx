70a06f3127f9c150ef897b61f2c073b2a1e11d87
BlockchainDataMiner <102978077+BlockchainDataMiner@users.noreply.github.com>
2023-03-09 15:58:58 +0000

    travail sur ui

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ export default function App() {
                 Welcome to Avax Royal Staker 2.1
             </Text>
 
-            <Container bg='black' m={2} maxWidth='30%'>
+            <Container bg='black' m={2} maxWidth='570px'>
                 <Grid templateColumns='repeat(2, 0fr)'>
                     <GridItem><AddNetworkButton/></GridItem>
                     <GridItem><ConnectButton/></GridItem>
